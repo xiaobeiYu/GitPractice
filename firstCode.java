680d0c22c4d27b6dabe494ec14bf8649256fb7d8
xiaobeiYu <xyu5@gwu.edu>
2016-10-17 15:36:17 -0700

    new change from branch1

--- firstCode.java
+++ firstCode.java
@@ -1,6 +1,7 @@
 //hello world
 public class Solution{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.print("this is from branch1");
     }
 }
